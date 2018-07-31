af31334619c96bae41e99fa4f52e99ec66587bec
Andreas Steingass <a.steingass@reply.de>
2018-07-31 15:01:50 +0200

    init commit, nothing configured yet

---
+++ example-ms/src/main/kotlin/com/example/examplems/ExampleMsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.examplems
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExampleMsApplication
+
+fun main(args: Array<String>) {
+    runApplication<ExampleMsApplication>(*args)
+}
